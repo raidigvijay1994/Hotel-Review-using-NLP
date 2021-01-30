5f0775502ff558c7097439030dff4d588a9b2d68
Digvijay Rai <raidigvijay1994@gmail.com>
2021-01-30 21:39:41 +0530

    Add all my files

---
+++ hotel_review.py
@@ -0,0 +1,153 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+# Natural Language Processing
+
+
+# In[1]:
+
+
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+# In[3]:
+
+
+pwd
+
+
+# In[4]:
+
+
+# Importing the dataset
+dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter = '\t', quoting = 3)
+
+
+# In[12]:
+
+
+dataset.head()
+
+
+# In[14]:
+
+
+dataset
+
+
+# In[5]:
+
+
+# Cleaning the texts
+import re
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+corpus = []
+for i in range(0, 1000):
+    review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][i])
+    review = review.lower()
+    review = review.split()
+    ps = PorterStemmer()
+    review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
+    review = ' '.join(review)
+    corpus.append(review)
+
+
+# In[15]:
+
+
+corpus
+
+
+# In[6]:
+
+
+# Creating the Bag of Words model
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features = 1500)
+X = cv.fit_transform(corpus).toarray()
+y = dataset.iloc[:, 1].values
+
+
+# In[16]:
+
+
+cv
+
+
+# In[17]:
+
+
+X
+
+
+# In[18]:
+
+
+y
+
+
+# In[20]:
+
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+
+
+# In[21]:
+
+
+X_train
+
+
+# In[22]:
+
+
+X_test
+
+
+# In[23]:
+
+
+# Training the Naive Bayes model on the Training set
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
+
+
+# In[24]:
+
+
+X_train
+
+
+# In[25]:
+
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+
+# In[26]:
+
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+
+# In[27]:
+
+
+print(cm)
+
